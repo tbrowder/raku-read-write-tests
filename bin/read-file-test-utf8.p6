e436ba313112b55582c0e1f849eb2b72c09fc067
Tom Browder <tom.browder@gmail.com>
2017-06-30 09:01:55 -0500

    create a utf8 version of both p5 and p6 and slim them down

---
+++ bin/read-file-test-utf8.p6
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl6
+
+
+
+
+
+my $debug = 0;
+
+if !@*ARGS.elems {
+  say qq:to/END/;
+  Usage: $*PROGRAM.basename <input file>
+  END
+  exit;
+}
+
+
+
+my $ifil = shift @*ARGS;
+
+die "FATAL: file '$ifil' not found.\n"
+    if !$ifil.IO.f;
+
+my $fsiz = $ifil.IO.s;
+
+say "  File '$ifil' size: $fsiz bytes" if $debug;
+
+
+
+my $nlines = 0;
+my $nchars = 0;
+for $ifil.IO.lines -> $line {
+    ++$nlines;
+    $nchars += $line.chars;
+}
+
+
+if $debug {
+    # adjust for newlines being removed
+    $nchars += $nlines;
+    say "  Normal end.";
+    say "  For input file '$ifil':";
+    say "    Number lines: $nlines";
+    say "    Number chars: $nchars";
+}
---
+++ bin/read-file-test-utf8.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+
+use v5.14; # 'say'
+# need to read with utf8 same as Perl 6
+use open IO => ':endoding(UTF-8)';
+use autodie;
+
+my $debug = 0;
+
+if (!@ARGV) {
+  print <<"HERE";
+Usage: $0 <input file>
+HERE
+
+  exit;
+}
+
+my $ifil = shift @ARGV;
+
+die "FATAL: file '$ifil' not found.\n"
+    if !-f $ifil;
+
+my $fsiz = -s $ifil;
+
+say "  File '$ifil' size: $fsiz bytes" if $debug;
+
+open my $fp, '<', $ifil;
+
+my $nlines = 0;
+my $nchars = 0;
+while (defined(my $line = <$fp>)) {
+  chomp $line;
+  ++$nlines;
+  $nchars += length $line;
+}
+
+if ($debug) {
+    # adjust for newlines being removed
+    $nchars += $nlines;
+    say "  Normal end.";
+    say "  For input file '$ifil':";
+    say "    Number lines: $nlines";
+    say "    Number chars: $nchars";
+}
